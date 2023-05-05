d603c2a3e7d172779517d1f24089564595667475
CJack <cjf1998@foxmail.com>
2023-05-05 15:22:36 +0800

    完善商品详情页

--- src/views/VDetails/comps/goodDetails/index.tsx
+++ src/views/VDetails/comps/goodDetails/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GoodInfo } from '@/src/views/VDetails/interface';
+import { nanoid } from 'nanoid';
 import classes from './goodDetails.module.scss';
 import GoodPrice from './comps/goodPrice';
 
@@ -33,26 +34,52 @@ const GoodDetails:React.FC<Props> = ({ goodInfo }) => {
             <span className={classes.support}>支持</span>
             <div className={classes.serverOpt}>
               <span>可配送全球</span>
+              <i>|</i>
               <span>99元免基础运费</span>
+              <i>|</i>
               <span>免举证退换货</span>
+              <i>|</i>
               <span>365天原厂维修</span>
             </div>
           </div>
           <div className={classes.logisServer}>
-            <div className={classes.logisIcon} />
             <div className={classes.serverOpt}>
+              {/* eslint-disable-next-line max-len */}
+              <img className={classes.logisIcon} src="https://img10.360buyimg.com/imagetools/jfs/t1/136750/17/12251/5189/5f8586e6E483b2f57/78ee33bdc7ef8c35.png" alt="京东物流" />
               <span>次日达</span>
+              <i>|</i>
               <span>预约送货</span>
+              <i>|</i>
               <span>部分收货</span>
+              <i>|</i>
               <span>送货上门</span>
             </div>
-            <h4 className={classes.logisIll}>
+            <span className={classes.logisIll}>
               由
               <span>京东</span>
               发货并提供售后服务，预计
-              <span>明天（05月06日）送达</span>
-            </h4>
+              <strong>明天（05月06日）</strong>
+              送达
+            </span>
           </div>
+        </div>
+      </div>
+      <div className={classes.cate}>
+        <span className={classes.cateName}>重量</span>
+        <div className={classes.cateVal}>
+          <span className={classes.support}>0.54kg</span>
+        </div>
+      </div>
+      <div className={classes.summaryLine} />
+      <div className={classes.cate}>
+        <div className={classes.cateName}>选择颜色</div>
+        <div className={classes.cateVal}>
+          {goodInfo.attr[0].color.map((item) => (
+            <div className={classes.goodColor} key={nanoid()}>
+              <img src={item.imgSrc} alt={item.name} />
+              <span>{item.name}</span>
+            </div>
+          ))}
         </div>
       </div>
     </div>
