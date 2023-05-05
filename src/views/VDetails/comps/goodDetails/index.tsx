334bc6e26d8ccc367b172f5bd15e86ad55408338
CJack <cjf19998@foxmail.com>
2023-05-05 11:24:14 +0800

    完善商品详情页

--- src/views/VDetails/comps/goodDetails/index.tsx
+++ src/views/VDetails/comps/goodDetails/index.tsx
@@ -16,24 +16,44 @@ const GoodDetails:React.FC<Props> = ({ goodInfo }) => {
         <span className={classes.title}>{goodTit}</span>
       </div>
       <GoodPrice price="4999.00" gifts={goodInfo.gifts} />
-      <div className={classes.cates}>
+      <div className={classes.cate}>
         <span className={classes.cateName}>增值业务</span>
-        <span className={classes.huishou}>高价回收，急速到账</span>
+        <div className={classes.cateVal}>
+          <span className={classes.huishou}>高价回收，急速到账</span>
+        </div>
       </div>
-      <div className={classes.cates}>
+      <div className={classes.cate}>
         <span className={classes.cateName}>配送至</span>
-        <div className={classes.logistics}>
+        <div className={classes.cateVal}>
           <div className={classes.addrInfo}>
-            <span className={classes.huishou}>湖南长沙市岳麓区</span>
-            <span>有货</span>
+            <div className={classes.addr}>湖南长沙市岳麓区</div>
           </div>
-          <div className={classes.logisInfo}>
-            <div className={classes.logisServer}>
-              <div className={classes.logisIcon} />
+          <strong className={classes.isEnough}>有货</strong>
+          <div className={classes.serverCate}>
+            <span className={classes.support}>支持</span>
+            <div className={classes.serverOpt}>
+              <span>可配送全球</span>
+              <span>99元免基础运费</span>
+              <span>免举证退换货</span>
+              <span>365天原厂维修</span>
             </div>
           </div>
+          <div className={classes.logisServer}>
+            <div className={classes.logisIcon} />
+            <div className={classes.serverOpt}>
+              <span>次日达</span>
+              <span>预约送货</span>
+              <span>部分收货</span>
+              <span>送货上门</span>
+            </div>
+            <h4 className={classes.logisIll}>
+              由
+              <span>京东</span>
+              发货并提供售后服务，预计
+              <span>明天（05月06日）送达</span>
+            </h4>
+          </div>
         </div>
-
       </div>
     </div>
   );
